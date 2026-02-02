946cdfcbcfba41fcec9dfa1a818c46ad98449662
nirholas <nirholas@users.noreply.github.com>
2026-02-02 09:21:47 +0000

    Fix mobile components index.ts to use correct exports from parent directory

--- src/components/mobile/index.ts
+++ src/components/mobile/index.ts
@@ -1,6 +1,6 @@
 // Mobile-optimized components
 // Components are in the parent directory
-export { default as BottomNav } from '../BottomNav';
+export { BottomNav } from '../BottomNav';
 export { default as SwipeableCard } from '../SwipeableCard';
 export { default as PullToRefresh } from '../PullToRefresh';
 export { default as FloatingActionButton } from '../FloatingActionButton';
